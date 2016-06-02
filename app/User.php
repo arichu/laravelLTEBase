3fa07ad01d3c4e92d46929b4f9e1fc92bf417c9e
SatoshiDark <ygon01@gmail.com>
2016-06-02 17:35:03 -0400

    Fixed error blade

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'type'
     ];
 
     /**
@@ -21,6 +21,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'type'
+        'password', 'remember_token',
     ];
 }
--- resources/views/layouts/partials/error.blade.php
+++ resources/views/layouts/partials/error.blade.php
@@ -1,9 +1,12 @@
-@if($errors->any())
+@if(count($errors) > 0)
     <div class="alert alert-danger alert-dismissible">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
         <h4><i class="icon fa fa-ban"></i> @lang('message.someproblems')</h4>
-        @foreach($errors->all() as $error)
-            <p>{{ $error }}</p>
-        @endforeach
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+
     </div>
 @endif
